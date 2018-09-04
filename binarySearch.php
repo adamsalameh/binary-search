c0b3a8fa3d3b61ffc024b6db87edf2f4663653e7
Adam <micro.83@hotmail.com>
2018-09-04 16:28:16 +0300

    Update binarySearch.php

--- binarySearch.php
+++ binarySearch.php
@@ -1,46 +1,45 @@
 <?php //php 7.0.8
-//             0  1  2  3   4   5   6   7   8   9  10  11  12  13/ 14  15/ 16  17  18/ 19  20  21/ 22  23/ 24
-$prime = array(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97);
 
-
-
-
-function mid($min,$max):int{
-    return ($min + $max)/2;
+function middle($left,$right):int{
+    return ($left + $right)/2;
 }
 
-
-function binary($array, $number){
-    $min = 0;
-    $max = count($array)-1;
-    $mid = mid($min,$max);
+function binarySearch($array, $key){
+    
+    $left = 0;
+    $right = count($array)-1;
     
     
-    while ($max >= $min){
-    if ($number == $array[$mid]){
+    
+    while ($left <= $right){
         
-        //echo $mid." ".$array[$mid]." ".$number;
-        return $mid;
+           $middle = middle($left,$right);
+        // $middle = (int)(($left + $right)/2);
+        
+    if ($key == $array[$middle]){
+        
+        
+        return $middle;
         
     }else
-    if ($number < $array[$mid]){
-       // echo $mid."\n";
-        $max = $mid-1;
-        $mid = mid($min,$max);
-        //echo $mid."\n";
-
+    if ($key < $array[$middle]){
+       
+        $right = $middle - 1;
+       
+      
     }else
-    if ($number > $array[$mid]){
-       // echo $mid."\n";
-        $min = $mid+1;
-        $mid = mid($min,$max);
+    if ($key > $array[$middle]){
+      
+        $left = $middle + 1;
+       
         
-       // echo $mid."\n";
-
+       
     } 
     }
 return -1;
 } 
 
-   echo (binary($prime,8)); 
+     //               0  1  2  3   4   5   6   7   8   9  10  11  12  13/ 14  15/ 16  17  18/ 19  20  21/ 22  23/ 24
+     $numbers = array(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97);
+     echo (binarySearch($numbers,79)); 
 ?>
