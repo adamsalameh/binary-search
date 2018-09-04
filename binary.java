530f7c03279d363cb00b4a0f83e729ad3e4434d1
Adam <micro.83@hotmail.com>
2018-09-04 13:48:00 +0300

    Update binary.java

--- binary.java
+++ binary.java
@@ -9,20 +9,23 @@ class Rextester
     public static int binarySearch(int[] array, int num){
         int min = 0;
         int max = array.length-1;
-        int mid = (min + max)/2;
         
-        while(max >= min){
+        
+        while(min <= max){
+            
+            int mid = (min + max)/2;
+            
             if (num == array[mid]){
                  return mid;
                 
                 
             }else if (num < array[mid]){
                max = mid -1;
-               mid = (min + max)/2;
+              
                 
             }else if (num > array[mid]){
                min = mid +1;
-               mid = (min + max)/2;                
+                      
             }          
             
         }
@@ -33,7 +36,7 @@ class Rextester
     {
         //             0  1  2  3   4   5   6   7   8   9  10  11  12  13/ 14  15/ 16  17  18/ 19  20  21/ 22  23/ 24
         int[] array = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
-        System.out.println(binarySearch(array,3));
+        System.out.println(binarySearch(array,19));
        
     }
 }
